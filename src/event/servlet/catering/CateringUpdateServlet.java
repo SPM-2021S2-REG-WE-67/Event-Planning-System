b12c52d5eb44e378b102f0a49c1dfe7bb2b1f1b3
soujanyaah <soujiakaru25@gmail.com>
2021-09-25 10:09:46 +0530

    Changes Made

--- src/event/servlet/catering/CateringUpdateServlet.java
+++ src/event/servlet/catering/CateringUpdateServlet.java
@@ -4,13 +4,6 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import catering.dao.CateringDao;
 import catering.model.Catering;
 
